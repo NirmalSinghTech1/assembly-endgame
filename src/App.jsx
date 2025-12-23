4f674fd8a4712a2c10b0b4a0612a6b9df3745e22
Nirmal Singh <nirmalsinghh007@gmail.com>
2025-12-23 20:23:13 +0530

    Implement user interface layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import alphabets from './Js/alphabets'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const word = 'refactor'.split('').map( (char, index) => {
+    return <span key={index} className='char'>{char.toUpperCase()}</span>
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className='header'>
+        <h1 className='title'>Assembly: Endgame</h1>
+        <p className='description'>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+        <div className='popup-message-container'>
+          <h2>You Win!</h2>
+          <p>Well done!🎉</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='languages-container'>
+        <span style={{backgroundColor: "#E2680F"}}>HTML</span>
+        <span style={{backgroundColor: "#328AF1"}}>CSS</span>
+        <span style={{backgroundColor: "#F4EB13", color: "#1E1E1E"}}>JavaScript</span>
+        <span style={{backgroundColor: "#2ED3E9", color: "#1E1E1E"}}>React</span>
+        <span style={{backgroundColor: "#298EC6"}}>TypeScript</span>
+        <span style={{backgroundColor: "#599137"}}>Node.js</span>
+        <span style={{backgroundColor: "#FFD742", color: "#1E1E1E"}}>Python</span>
+        <span style={{backgroundColor: "#D02B2B"}}>Ruby</span>
+        <span style={{backgroundColor: "#2D519F"}}>Assembly</span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className='word-container'>
+       {word}
+      </div>
+      <div className='keyboard'>
+        {alphabets.map( item => {
+          return <button key={item}>{item}</button>
+        })}
+      </div>
+      <button className='new-game-button'>New Game</button>
+    </main>
   )
 }
 
