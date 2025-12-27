be705c09c10b00732797a275900d21971891108b
Nirmal Singh <nirmalsinghh007@gmail.com>
2025-12-27 16:44:11 +0530

    Add A11y functionality and improve code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Confetti from 'react-confetti'
 import clsx from 'clsx'
 import './App.css'
 import languages from './Js/languages'
 import getFarewellMessage from './Js/utils'
 import { getRandomWord } from './Js/utils'
 
-// Static Variables 
+// Static Variable
 const alphabets =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 function App() {
@@ -35,6 +36,7 @@ function App() {
         setGuessedLetters([])
     }
 
+    // Show farewell messages when the user guessed wrong letter
     function farwellMessages() {
         if(!isGameOver) {
             return (
@@ -68,7 +70,6 @@ function App() {
             backgroundColor: item.bgColor,
             color: item.color
         }
-
         const className = clsx('chip', {
             wrong: index < wrongGuessCount
         })
@@ -100,51 +101,68 @@ function App() {
             correct: guessedLetters.includes(letter) && currentWord.split('').includes(letter),
             incorrect: guessedLetters.includes(letter) && !currentWord.includes(letter)
         })
-
         return (
             <button 
                 onClick={() => handleGuessedLetter(letter)} 
                 key={letter}
                 className={className}
+                aria-disabled={guessedLetters.includes(letter)}
+                aria-label={`letter ${letter}`}
             >{letter.toUpperCase()}
             </button>
         )
     })
-
+    
     return (
         <main>
+            {isGameWon ? <Confetti numberOfPieces={300} /> : null}
+            
             {/* Header Section */}
             <section className="header">
                 <h1>Assembly Endgame</h1>
                 <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
             </section>
 
-            {/* Farewell message to indicate if the guess is wrong */}
-            <section className="farewell-message">
+            {/* Farewell message to indicate if the guess doesn't match */}
+            <section className="farewell-message" aria-live='polite' role='status'>
                 {isGuessWrong || isGameOver ? farwellMessages() : null}
             </section>
-
+            
             {/* Languages Section*/}
             <section className="language-chips">
                 {languageElements}
             </section>
 
             {/* Guess Word Section */}
-            <section className="word">
+            <section className="word" aria-live='polite'>
                 {wordElements}
+            </section>
+            
+            {/* Status section for screen readers only */}
+            <section className='sr-only' role='status' aria-live='polite'>
+                <p>
+                    {currentWord.includes(guessedLetters[guessedLetters.length-1]) ?
+                        `Correct! The letter ${guessedLetters[guessedLetters.length-1]} is in the word` :
+                        `Sorry! The letter ${guessedLetters[guessedLetters.length-1]} is not in the word`
+                    }
+                    You have {(languages.length-1) - wrongGuessCount} attempts left.
+                </p>
+                <p>Current Word: {currentWord.split('').map(letter => 
+                    guessedLetters.includes(letter) ? letter : "blank"
+                ).join(' ')}</p>
             </section>
 
             {/* Keyboard Section */}
             <section 
                 className={clsx('keyboard', {
-                disable: wrongGuessCount === languages.length - 1
-            })}
+                disable: isGameOver
+                })}
             >
                 {keyboard}
             </section>
 
             {/* New Game Button */}
-            <section className='new-game'>
+            <section className='new-game' aria-live='polite'>
                 {isGameOver ? <button onClick={startNewGame} className='new-game-btn'>New Game</button> : null}
             </section>
         </main>
